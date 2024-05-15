a5733a2b9cf332f0bab91ee2c578e5d8f5f4c293
gabrielguivan <36721955+gabrielguivan@users.noreply.github.com>
2024-05-15 02:51:19 -0300

    JPEG

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import profilePic from '../public/foto.jpeg'
+import profilePic from '../public/foto.JPEG'
 import Link from 'next/link'
 
 export default function Home() {
