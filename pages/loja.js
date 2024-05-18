a6529aa7a19f199b087066691fa286d0583a5b7b
gabrielguivan <36721955+gabrielguivan@users.noreply.github.com>
2024-05-18 18:29:10 -0300

    teste redeploy

--- pages/loja.js
+++ pages/loja.js
@@ -1,46 +1,37 @@
 // pages/loja.js
-import React from 'react';
 import axios from 'axios';
-import Link from 'next/link';
 
 export async function getServerSideProps() {
+  const baseUrl = process.env.NEXT_PUBLIC_URL;
+
   try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_URL}/api/produtos`);
-    const products = res.data;
+    const productsRes = await axios.get(`${baseUrl}/api/produtos`);
+    const products = productsRes.data;
 
     return {
-      props: {
-        products,
-      },
+      props: { products },
     };
   } catch (error) {
-    console.error("Error fetching products:", error);
+    console.error('Error fetching products:', error);
     return {
-      props: {
-        products: [],
-      },
+      props: { products: [] },
     };
   }
 }
 
 const Loja = ({ products }) => {
-  const addToCart = (product) => {
-    // Logic to add product to cart
-  };
-
   return (
-    <div className="container">
+    <div>
       <h1>Loja</h1>
       <div className="product-gallery">
         {products.length > 0 ? (
           products.map((product) => (
             <div key={product.id} className="product-card">
-              <img src={product.image} alt={product.name} className="product-image" />
+              <img src={product.images[0]} alt={product.name} className="product-image" />
               <div className="product-info">
-                <h2>{product.name}</h2>
+                <h3>{product.name}</h3>
                 <p>{product.description}</p>
                 <p>R${(product.price / 100).toFixed(2)}</p>
-                <button onClick={() => addToCart(product)}>Adicionar ao Carrinho</button>
               </div>
             </div>
           ))
@@ -48,41 +39,6 @@ const Loja = ({ products }) => {
           <p>Nenhum produto encontrado.</p>
         )}
       </div>
-      <style jsx>{`
-        .container {
-          padding: 2rem;
-        }
-        .product-gallery {
-          display: flex;
-          flex-wrap: wrap;
-          gap: 2rem;
-        }
-        .product-card {
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          padding: 1rem;
-          width: calc(33.333% - 2rem);
-        }
-        .product-image {
-          width: 100%;
-          height: auto;
-        }
-        .product-info {
-          text-align: center;
-        }
-        button {
-          background: var(--primary-color);
-          color: var(--dark-text-color);
-          border: none;
-          padding: 0.5rem 1rem;
-          border-radius: 5px;
-          cursor: pointer;
-          transition: background 0.3s ease;
-        }
-        button:hover {
-          background: var(--hover-color);
-        }
-      `}</style>
     </div>
   );
 };
