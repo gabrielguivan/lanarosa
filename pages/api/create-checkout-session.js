aca91684652795ccb3487d7a7b063925c802e1ec
gabrielguivan <36721955+gabrielguivan@users.noreply.github.com>
2024-05-19 00:52:14 -0300

    mais test

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,4 +1,3 @@
-// pages/api/create-checkout-session.js
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
@@ -11,7 +10,7 @@ export default async (req, res) => {
         success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/cancel`,
       });
-      res.status(200).json({ id: session.id });
+      res.status(200).json({ id: session.url });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
